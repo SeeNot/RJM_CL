4b1fee72a9e9b53e7a8ad15275e8bffac87b76f2
SeeNot <martins2004b@gmail.com>
2023-09-23 18:44:22 +0300

    Start

---
+++ RJM_CL/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace RJM_CL
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string dire1;
+            string dire2;
+
+
+            string[] entryOne = Directory.GetFiles("D:\\Martins\\real imiges\\test1");
+
+            foreach (var fil1 in entryOne)
+            {
+                Console.WriteLine(fil1);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
