cebcee11bb4b60426f6134f4a92f2c4ecb478770
SeeNot <martins2004b@gmail.com>
2023-09-23 22:32:09 +0300

    Made the code shorter

--- RJM_CL/Program.cs
+++ RJM_CL/Program.cs
@@ -9,6 +9,8 @@ namespace RJM_CL
     {
         public static void Main(string[] args)
         {   
+            Console.WriteLine("Hello and welcome to the RJM! \nTo continue press ENTER");
+            Console.ReadLine();
             Console.WriteLine("Enter address of first folder!");
             string dire1 = Console.ReadLine();
             Console.WriteLine("Enter address of second folder!");
@@ -22,31 +24,33 @@ namespace RJM_CL
             string[] entryOne = Directory.GetFiles(dire1);
             string[] entryTwo = Directory.GetFiles(dire2);
 
-            for (int i = 0; i < entryOne.Length; i++)
-            {
-                entryOne[i] = entryOne[i].Remove(0, dire1.Length + 1);
-                entryOne[i] = entryOne[i].Replace("." + extension1, "");
-            }
 
-            List<string> first = entryOne.ToList();
-            for (int i = 0; i < entryTwo.Length; i++)
-            {
-                entryTwo[i] = entryTwo[i].Remove(0, dire2.Length + 1);
-                entryTwo[i] = entryTwo[i].Replace("." + extension2, "");
-            }
+            List<string> first = ListMaker(entryOne, extension1, dire1.Length);
+            List<string> second = ListMaker(entryTwo, extension2, dire2.Length);
 
-            List<string> second = entryTwo.ToList();
 
 
             var unnecessaryRaws = second.Except(first).ToList();
             foreach (var kle in unnecessaryRaws)
             {
-                Console.WriteLine(kle);
                 Console.WriteLine(dire2+"\\"+kle+"."+extension2);
                 //File.Delete(dire2+"\\"+kle+"."+extension2);
             }
 
 
         }
+
+        private static List<string> ListMaker(string[] entry, string extension, int lenght)
+        {
+            
+            for (int i = 0; i < entry.Length; i++)
+            {
+                entry[i] = entry[i].Remove(0, lenght + 1);
+                entry[i] = entry[i].Replace("." + extension, "");
+            }
+            
+            return entry.ToList();
+        }
+        
     }
 }
